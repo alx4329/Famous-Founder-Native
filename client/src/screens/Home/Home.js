4dde70122a6a79c86bef282190a59bb92d7a70ad
Alexis Martinez <alexis4329@gmail.com>
2022-04-26 17:03:27 -0300

    [feature] toma foto con la camara y galeria

--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -1,13 +1,62 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Modal, Pressable, Image } from 'react-native';
 import {
     responsiveHeight,
     responsiveWidth,
     } from "react-native-responsive-dimensions";
 import DragZone from '../../components/DragZone.js';
-
-
+import * as ImagePicker from 'expo-image-picker'
+import camera from '../../assets/icons/photo_camera.png';
+import gallery from '../../assets/icons/gallery.png';
+const includeExtra = true;
 const Home = ()=>{
+    const [showModal, setShowModal] = React.useState(false);
+    const [ response, setResponse] = React.useState(null);
+    
+    const [status, requestPermission] = ImagePicker.useCameraPermissions();
+
+    const openGallery = async()=>{
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+          });
+      
+          console.log(result);
+      
+          if (!result.cancelled) {
+            setResponse(result.uri);
+          }
+        };
+    
+        
+    const launchCamera=async()=>{
+        let result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+          });
+      
+          console.log(result);
+      
+          if (!result.cancelled) {
+            setResponse(result.uri);
+          }
+
+    }
+    const openCamera =  async() =>{
+        if(status.granted){
+            launchCamera();
+        } else{
+            await ImagePicker.requestCameraPermissionsAsync()
+            launchCamera();
+            
+        }
+        
+    }
+
     return(
         <View style={styles.container} >
             <View style={styles.header} >
@@ -17,10 +66,51 @@ const Home = ()=>{
             <View style={styles.body} >
                 <Text style={styles.title}>¿Quién es el famoso?</Text>
             </View>
-            <View style={styles.dragzone} >
-            <DragZone/>
+                <Pressable 
+                    onPress={()=>setShowModal(true)}
+                >
+                    <View style={styles.dragzone} >
+                        <DragZone/>
+                    </View>
+                </Pressable>
+            {
+                showModal &&
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={showModal}
+                    onRequestClose={() => {
+                        Alert.alert("Modal has been closed.");
+                        setShowModal(false)
+                    }}
+                >
+                    <View style={styles.modalPosition}>
+                        <View style={styles.modalView}>
+                            <View style={styles.topBar} />
+                            
 
-            </View>
+                            <Text style={styles.modalTitle}>Selecciona una foto</Text>
+                            <Pressable
+                                style={[styles.button]}
+                                onPress={openGallery}
+                            >
+                                <Image style={styles.icon} source={gallery} />
+                                <Text style={styles.textStyle}>Galeria de fotos</Text>
+                            </Pressable>
+
+                            <Pressable
+                                style={[styles.button]}
+                                onPress={openCamera}
+                            >
+                                <Image style={styles.icon} source={camera} />
+                                <Text style={styles.textStyle}>Cámara</Text>
+                            </Pressable>
+                            <View style={styles.bottomBar} />
+
+                        </View>
+                    </View>
+                </Modal>
+            }
         </View>
         
     )
@@ -67,7 +157,74 @@ const styles = StyleSheet.create({
         fontSize:responsiveHeight(2.6),
         color:'#0F172A',
 
-    }
+    },
+    modalPosition: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        
+        backgroundColor: "rgba(0,0,0,0.5)",
+        width: '100%',
+      },
+        modalView: {
+            width: '100%',
+            height: '27%',
+            backgroundColor: "#fff",
+            borderTopLeftRadius: responsiveWidth(6),
+            borderTopRightRadius: responsiveWidth(6),
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 2
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 5,
+            position: 'absolute',
+            bottom:0,
+            paddingVertical:responsiveHeight(4),
+        },
+        modalTitle: {
+            fontSize: responsiveHeight(2.2),
+            fontFamily: 'roboto-bold',
+            color: '#64748B',
+            marginBottom: responsiveHeight(1),
+        },
+        textStyle: {
+            marginLeft: responsiveWidth(2),
+            fontFamily: "roboto-bold",
+            color: "#0F172A",
+            fontSize: responsiveHeight(2.2),
+          },
+          icon:{
+            width:responsiveWidth(6),
+            height:responsiveWidth(6),
+        },
+        button:{
+            paddingHorizontal:responsiveWidth(7),
+            paddingVertical:responsiveHeight(1.5),
+            alignItems:'center',
+            width:responsiveWidth(100),
+            flex:1,
+            flexDirection:'row',
+        },
+        topBar:{
+            position:'absolute',
+            height:responsiveHeight(0.7),
+            width:responsiveWidth(13),
+            backgroundColor:'#F1F5F9',
+            top:responsiveHeight(0.7),
+            borderRadius:responsiveHeight(0.5),
+        },
+        bottomBar:{
+            position:'absolute',
+            height:responsiveHeight(0.7),
+            width:'37%',
+            backgroundColor:'#0F172A',
+            bottom:responsiveHeight(0.7),
+            borderRadius:responsiveHeight(0.5),
+        }
 })
 
 export default Home;
